4299dab05afe7cf9d80c7d33dd23108930d1824d
yamatoto <mftb35ymt@yahoo.co.jp>
2021-10-25 12:38:35 +0900

    form作成

---
+++ src/components/meetups/NewMeetupForm.tsx
@@ -0,0 +1,59 @@
+import React, { useRef } from "react";
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
+
+const NewMeetupForm = () => {
+  const titleInputRef = useRef<HTMLInputElement>(null);
+  const imageInputRef = useRef<HTMLInputElement>(null);
+  const addressInputRef = useRef<HTMLInputElement>(null);
+  const descriptionInputRef = useRef<HTMLTextAreaElement>(null);
+
+  function submitHandler(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const enteredTitle = titleInputRef.current!.value;
+    const enteredImage = imageInputRef.current!.value;
+    const enteredAddress = addressInputRef.current!.value;
+    const enteredDescription = descriptionInputRef.current!.value;
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    console.log(meetupData);
+  }
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={imageInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Meetup Address</label>
+          <input type="text" required id="address" ref={addressInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Meetup Description</label>
+          <textarea
+            required
+            id="description"
+            rows={5}
+            ref={descriptionInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default NewMeetupForm;
--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,6 +1,6 @@
 import classes from "./Card.module.css";
 
-const Card = ({ children }: { children: JSX.Element[] }) => {
+const Card = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
   return <div className={classes.card}>{children}</div>;
 };
 
